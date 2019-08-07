ddd589298335356f50e620eef278e2c6f1d1a25c
Alex Huk <alex.huk85@gmail.com>
2019-08-07 13:03:47 +1000

    Pop up Window

--- mapping/map1.py
+++ mapping/map1.py
@@ -1,10 +1,25 @@
 import folium
-map = folium.Map(location=[-33.868233, 151.208067])
+import pandas
+
+# Read data from file
+data = pandas.read_csv('Volcanoes.txt')
+lat = list(data['LAT'])
+lon = list(data['LON'])
+elev = list(data['ELEV'])
+
+html = """
+    <h4>Volcano information:</h4>
+    Height: %s m
+"""
+# Base of map + starting point
+map = folium.Map(location=[-33.892055, 150.959063], zoom_start=3)
 
 fg = folium.FeatureGroup(name='My Map')
 
-for coordinates in [[-33.8, 151.3],[-34, 150]]:
-    fg.add_child(folium.Marker(location=coordinates, popup='Hi i am marker', icon=folium.Icon(color='green')))
+# Loop + add point from data location
+for lt, ln, el in zip(lat, lon, elev):
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
